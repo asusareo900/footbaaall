9c40d361826754b3aabe6845d693eb392049e5ad
asusareo900 <deeganoisin1@gmail.com>
2025-02-05 14:17:52 +0000

    added winning

--- main.ts
+++ main.ts
@@ -5,23 +5,28 @@ namespace SpriteKind {
     export const field = SpriteKind.create()
     export const Bar = SpriteKind.create()
     export const selector = SpriteKind.create()
+    export const Feild2 = SpriteKind.create()
+    export const Field3 = SpriteKind.create()
 }
-/**
- * use the overlap triggers to make the power bar work
- */
-let kick5 = 0
-let mySprite12: Sprite = null
-let kick4 = 0
-let red: Sprite = null
-let orange: Sprite = null
-let yellow: Sprite = null
-let green: Sprite = null
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Field3, function (sprite, otherSprite) {
+    game.setGameOverEffect(false, effects.slash)
+    game.setGameOverMessage(false, "OH WELL...")
+    game.gameOver(false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Feild2, function (sprite, otherSprite) {
+    game.setGameOverEffect(true, effects.confetti)
+    game.setGameOverMessage(true, "NICE SHOT!")
+    game.gameOver(true)
+})
 let kick3 = 0
 let kick2 = 0
+let mySprite12: Sprite = null
+let mySprite11: Sprite = null
 let mySprite10: Sprite = null
 let mySprite9: Sprite = null
-let mySprite7: Sprite = null
 let mySprite8: Sprite = null
+let myDart2: Dart = null
+let mySprite7: Sprite = null
 let mySprite2 = sprites.create(assets.image`myImage`, SpriteKind.field)
 mySprite2.setScale(10, ScaleAnchor.Middle)
 let mySprite3 = sprites.create(assets.image`Goal0`, SpriteKind.Goal)
@@ -49,59 +54,39 @@ forever(function () {
         sprites.destroyAllSpritesOfKind(SpriteKind.Ball)
         sprites.destroyAllSpritesOfKind(SpriteKind.field)
         pause(1000)
-        mySprite8 = sprites.create(assets.image`myImage3`, SpriteKind.field)
-        mySprite8.changeScale(10, ScaleAnchor.Middle)
-        mySprite7 = sprites.create(assets.image`myImage2`, SpriteKind.Goal)
+        scene.setBackgroundImage(assets.image`myImage3`)
+        mySprite7 = sprites.create(assets.image`myImage14`, SpriteKind.Goal)
         mySprite7.changeScale(-0.5, ScaleAnchor.Middle)
         mySprite7.setPosition(80, 15)
-        mySprite9 = sprites.create(assets.image`myImage1`, SpriteKind.Ball)
-        mySprite9.setPosition(80, 103)
-        mySprite10 = sprites.create(assets.image`myImage4`, SpriteKind.Bar)
-        mySprite10.setPosition(80, 78)
-        mySprite10.setScale(4.8, ScaleAnchor.Middle)
-        pause(100)
-        mySprite9.setVelocity(100, 0)
-        mySprite9.setBounceOnWall(true)
+        myDart2 = darts.create(assets.image`myImage1`, SpriteKind.Player, randint(10, 150), 110)
+        myDart2.controlWithArrowKeys(true)
+        myDart2.setTrace(true)
+        mySprite8 = sprites.create(assets.image`myImage13`, SpriteKind.Goal)
+        mySprite8.changeScale(-0.5, ScaleAnchor.Middle)
+        mySprite8.setPosition(80, 15)
+        mySprite9 = sprites.create(assets.image`myImage2`, SpriteKind.Goal)
+        mySprite9.changeScale(-0.5, ScaleAnchor.Middle)
+        mySprite9.setPosition(80, 15)
+        mySprite10 = sprites.create(assets.image`myImage15`, SpriteKind.Feild2)
+        mySprite10.setPosition(80, 1)
+        mySprite11 = sprites.create(assets.image`myImage16`, SpriteKind.Field3)
+        mySprite11.setPosition(34, 1)
+        mySprite11.setScale(2, ScaleAnchor.Middle)
+        mySprite12 = sprites.create(assets.image`myImage16`, SpriteKind.Field3)
+        mySprite12.setPosition(126, 1)
+        mySprite12.setScale(2, ScaleAnchor.Middle)
         kick2 = 1
     }
 })
 forever(function () {
-    if (controller.B.isPressed() && kick2 == 1) {
+    if (kick2 == 1 && controller.B.isPressed()) {
         kick2 = 0
-        mySprite9.setVelocity(0, 0)
-        sprites.destroyAllSpritesOfKind(SpriteKind.Bar)
+        myDart2.throwDart()
         kick3 = 1
     }
 })
 forever(function () {
-    if (controller.B.isPressed() && kick3 == 1) {
+    if (kick3 == 1 && controller.A.isPressed()) {
         kick3 = 0
-        green = sprites.create(assets.image`myImage7`, SpriteKind.Bar)
-        green.setScale(4.3, ScaleAnchor.Middle)
-        green.setPosition(13, 60)
-        yellow = sprites.create(assets.image`myImage8`, SpriteKind.Bar)
-        yellow.setScale(4.3, ScaleAnchor.Middle)
-        yellow.setPosition(13, 60)
-        orange = sprites.create(assets.image`myImage9`, SpriteKind.Bar)
-        orange.setScale(4.3, ScaleAnchor.Middle)
-        orange.setPosition(13, 60)
-        red = sprites.create(assets.image`myImage10`, SpriteKind.Bar)
-        red.setScale(4.3, ScaleAnchor.Middle)
-        red.setPosition(13, 60)
-        kick4 = 1
-    }
-    if (controller.B.isPressed() && kick4 == 1) {
-        kick4 = 0
-        mySprite12 = sprites.create(assets.image`myImage6`, SpriteKind.selector)
-        mySprite12.setScale(0.5, ScaleAnchor.Middle)
-        mySprite12.setPosition(13, 60)
-        mySprite12.setVelocity(0, 100)
-        mySprite12.setBounceOnWall(true)
-        kick5 = 1
-    }
-    pause(100)
-    if (controller.B.isPressed() && kick5 == 1) {
-        kick5 = 0
-        mySprite12.setVelocity(0, 0)
     }
 })
